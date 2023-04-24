68546ca3e50b5055fe96271445f2214a9c5cedf7
Karim Gad <93051825+Xer0-K@users.noreply.github.com>
2023-04-24 21:48:19 +0200

    Delete EmailValidation.js

--- Modules(js)/EmailValidation.js
+++
@@ -1,9 +0,0 @@
-function validateEmail() {
-    const input = document.querySelector("#example");
-    const display = document.querySelector("#result");
-    if (input.value.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi)) {
-        display.innerHTML = input.value + ' is valid';
-    } else {
-        display.innerHTML = input.value + ' is not a valid email';
-    }
-}
