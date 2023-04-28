37d24f120357b6781879dce7c362112ff7497683
Karim Gad <93051825+Xer0-K@users.noreply.github.com>
2023-04-28 17:14:51 +0200

    Delete PasswordValidation.js

--- Modules(js)/PasswordValidation.js
+++
@@ -1,9 +0,0 @@
-function validatePassword(password) {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-    if (password.match(regex)) {
-        alert("Password is valid");
-    } else {
-        alert("Password is invalid");
-    }
-}
