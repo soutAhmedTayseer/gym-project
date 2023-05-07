fb2e22fbd09ec810dbdb307ea371fd70d9231da9
moatasem0-0 <121245335+moatasem0-0@users.noreply.github.com>
2023-05-08 01:04:25 +0300

    Add files via upload
    mo3 mo3

---
+++ myproject/templates-php/errordialog.php
@@ -0,0 +1,10 @@
+<?php
+echo "<script>
+    function errdia(divid,text)
+    {
+        errdiv= document.getElementById(divid);
+        errdiv.hidden= false;
+        errdiv.children[0].innerHTML =text;
+    }
+    </script>"
+    ?>
