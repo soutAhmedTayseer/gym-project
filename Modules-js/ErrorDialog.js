c3a8e8ee399f76628f8ff5e1869075576acaae99
ironnoid <131486175+ironnoid@users.noreply.github.com>
2023-05-18 20:46:01 +0300

    Delete Modules-js directory

--- Modules-js/ErrorDialog.js
+++
@@ -1,5 +0,0 @@
-function errdia(divid, text) {
-    errdiv = document.getElementById(divid);
-    errdiv.hidden = false;
-    errdiv.children[0].innerHTML = text;
-}
--- Modules-js/PhoneNoValidation.js
+++
@@ -1,3 +0,0 @@
-function isPhoneNumber(input) {
-    return /^\d{11}$/.test(input);
-}
