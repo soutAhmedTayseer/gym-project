0f4f4bd645f3ebb3678bbd330f0e7c4fc84751e1
ironnoid <131486175+ironnoid@users.noreply.github.com>
2023-05-18 20:45:37 +0300

    Delete templates-php directory

--- templates-php/EmailValidation.php
+++
@@ -1,6 +0,0 @@
-<?php
-function isValidEmail($email)
-{
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-?>
--- templates-php/PhoneNoValidation.php
+++
@@ -1,6 +0,0 @@
-<?php
-function isValidTel($phoneNumber)
-{
-    return preg_match('/^\d{11}$/', $phoneNumber);
-}
-?>
